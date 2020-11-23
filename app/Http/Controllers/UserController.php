11c49d3d4fb6d82e50314786caef9983b6a94508
Tyro Studio <tyrostudioofficial@gmail.com>
2020-11-23 06:39:14 +0600

    login and session check

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    function login(Request $req){
+        $user = User::where(["email"=>$req->email])->first();
+        if($user && Hash::check($req->password, $user->password)){
+            $req->session()->put('user',$user);
+           return redirect('/');
+        } else {
+            return "user pass no match";
+        }
+    }
+}
